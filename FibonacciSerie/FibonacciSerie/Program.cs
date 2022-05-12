0f526c03f71680fd918e1e5c02d7cff2e847bd75
julioconceicao <julioconceicao.gtr@gmail.com>
2022-05-11 23:15:58 -0300

    [Update May.11.2022]

---
+++ FibonacciSerie/FibonacciSerie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciSerie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1;
+            int n2 = 0;
+            int soma = 0;
+
+            for (var i = 0; i <= 100; i++)
+            {
+                soma = n1 + n2;
+                n2 = n1;
+                n1 = soma;
+                Console.Write($"{soma}, ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
